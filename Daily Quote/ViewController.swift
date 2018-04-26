c8acd31d2bb29fc8135daa074dd3d0e5ac2e7840
Max Awad <maxmax2012@hotmail.com>
2018-04-26 13:04:30 -0400

    Initial Commit

---
+++ Daily Quote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Daily Quote
+//
+//  Created by Moustafa Awad on 4/26/18.
+//  Copyright © 2018 Moustafa Awad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
