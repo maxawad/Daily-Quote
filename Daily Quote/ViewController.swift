668e28d56e2ed465f9ef62441e48bf265267f49e
Max Awad <maxmax2012@hotmail.com>
2018-06-21 22:13:45 -0400

    Updated a bunch of stuff
    - Added walkthough like swipe animation.
    - Added Welcome Screen (pending gif/loitte animation)
    - removed next button (can swipe back and forwards now)
    - Small tweaks to icons placement.

--- Daily Quote/ViewController.swift
+++ Daily Quote/ViewController.swift
@@ -19,13 +19,15 @@ class ViewController: UIViewController {
 
     // MARK: - Outlets
     
+    @IBOutlet weak var popoverOutlet: UIView!
+    
     @IBOutlet weak var TimePickerViewOutlet: UIView!
+    
     @IBOutlet weak var TimePickerOutlet: UIDatePicker!
     @IBOutlet weak var quoteText: UILabel!
     @IBOutlet weak var quoteAuthor: UILabel!
     @IBOutlet weak var bgImageView: UIImageView!
     @IBOutlet weak var shareButtonOutlet: UIButton!
-    @IBOutlet weak var NextButtonOutlet: UIButton!
     @IBOutlet weak var gearButtonOutlet: UIButton!
     @IBOutlet weak var alarmButtonOutlet: UIButton!
     @IBOutlet weak var visualEffectView: UIVisualEffectView!
@@ -36,61 +38,108 @@ class ViewController: UIViewController {
     @IBOutlet weak var visualEffectInApp: UIVisualEffectView!
     @IBOutlet weak var RestoreButtonOutlet: UIButton!
     
+    @IBOutlet var NewView: UIView!
+    @IBOutlet weak var newButtonOutlet: UIButton!
+    @IBOutlet weak var newVisualOutlet: UIVisualEffectView!
+    
+    // MARK: - Swipe Gesture Recognizer Outlet and Action
+    
+    func addSwipeHandler() {
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
+        
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
+        leftSwipe.direction = .left
+        
+        view.addGestureRecognizer(rightSwipe)
+        view.addGestureRecognizer(leftSwipe)
+
+    }
+    
+    @objc func handleSwipe(sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            if feed != nil{
+                switch sender.direction {
+                case .right:
+                    nextQuote()
+                case .left:
+                    prevQuote()
+                default:
+                    break
+                }
+            } else {
+                let alert = UIAlertController(title: "No internet!", message: "Cannot establish a connection to the internet.", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Dismiss", style: .destructive, handler: { action in
+                    print("destructive")
+                    
+                }))
+                self.present(alert, animated: true, completion: nil)
+
+            }
+        }
+        
+    }
     
     // MARK: - Actions
     
-    @IBAction func gearButton(_ sender: Any) {
-        animateIn2()
-    }
-    @IBAction func alarmButton(_ sender: Any) {
-        animateIn()
+    @IBAction func dismissWelcome(_ sender: Any) {
+        animateOut(uiview: NewView)
+        
     }
     
-    @IBAction func NextButton(_ sender: Any) {
-        
-        if feed != nil{
-                    rotateQuote()
-        } else {
-            let alert = UIAlertController(title: "No internet!", message: "Cannot establish a connection to the internet.", preferredStyle: UIAlertControllerStyle.alert)
-            alert.addAction(UIAlertAction(title: "Dismiss", style: .destructive, handler: { action in
-                print("destructive")
-                
-            }))
-            self.present(alert, animated: true, completion: nil)
-        }
-        
-        
-        UIView.animate(withDuration: 1.25, delay: 0.3, animations: {
-            self.NextButtonOutlet.alpha = 0
-        })
+    @IBAction func gearButton(_ sender: Any) {
+        animateIn(uiview: inAppViewOutlet)
     }
+    @IBAction func alarmButton(_ sender: Any) {
+        animateIn(uiview: TimePickerViewOutlet)
+    }
+    
+//    @IBAction func NextButton(_ sender: Any) {
+//
+//        if feed != nil{
+//                    rotateQuote()
+//        } else {
+//            let alert = UIAlertController(title: "No internet!", message: "Cannot establish a connection to the internet.", preferredStyle: UIAlertControllerStyle.alert)
+//            alert.addAction(UIAlertAction(title: "Dismiss", style: .destructive, handler: { action in
+//                print("destructive")
+//
+//            }))
+//            self.present(alert, animated: true, completion: nil)
+//        }
+//    }
     
     @IBAction func ShareButton(_ sender: Any) {
         DispatchQueue.main.async {
             let activityVC = UIActivityViewController(activityItems: ["\"" + self.quoteText.text! + "\"", "\n- " + self.quoteAuthor.text!], applicationActivities: nil)
-        
+            if (UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.pad)
+            {
+                //ios > 8.0
+                if ( activityVC.responds(to: #selector(getter: UIViewController.popoverPresentationController)) ) {
+                    activityVC.popoverPresentationController?.sourceView = self.popoverOutlet
+                }
+            }
             self.present(activityVC, animated: true, completion: nil)
         }
+        
     }
     
     @IBAction func ScreenButton(_ sender: Any) {
         self.quoteText.alpha = 1.0
         self.shareButtonOutlet.alpha = 1.0
-        self.NextButtonOutlet.alpha = 1.0
         self.gearButtonOutlet.alpha = 1.0
         self.alarmButtonOutlet.alpha = 1.0
     }
     
     @IBAction func doneButton(_ sender: Any) {
-        animateOut()
+        animateOut(uiview: self.TimePickerViewOutlet)
         let rtnString = self.quoteText.text! + "\n - " + self.quoteAuthor.text!
         appDelegate.saveDate(date: TimePickerOutlet.date)
         appDelegate.scheduleNotification(quote: rtnString, date: TimePickerOutlet.date)
     }
     
     @IBAction func dismissButton(_ sender: Any) {
-        animateOut()
-        animateOut2()
+        animateOut(uiview: self.NewView)
+        animateOut(uiview: self.TimePickerViewOutlet)
+        animateOut(uiview: self.inAppViewOutlet)
     }
     @IBAction func IAPButton(_ sender: Any) {
         IAPService.shared.purchase(product: .autoRenewing)
@@ -113,6 +162,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        UserDefaults.standard.set(false, forKey: "showedWelcome")
+        
+        
         IAPService.shared.getProducts()
         
         
@@ -121,12 +173,16 @@ class ViewController: UIViewController {
         askNotificationPermissions()
         getQuotes()
         
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         playCoolIntro()
+        animateInFrame(uiview: NewView)
+        isFirstTime()
+        addSwipeHandler()
     }
     
 
@@ -138,8 +194,8 @@ class ViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
-
-
+    
+    
     
     // MARK: Custom Methods
     
@@ -150,30 +206,66 @@ class ViewController: UIViewController {
                 print("Successful Authorization")
             }
         }
-        
     }
     
+    func nextQuote(){
+        rotateQuote()
+    }
+    
+    func prevQuote(){
+        count = (count - 1)
+        if count == -1 {
+            count = 3
+        }
+        let item = self.feed?.items?[count]
+        let author:String = (item!.title)!
+        var quoting = (item!.description)!
+        quoting = String(quoting.dropLast())
+        quoting = String(quoting.dropFirst())
+        let quote: String = quoting
+        DispatchQueue.main.async {
+            // ..and update the UI
+            self.quoteText.text = quote
+            self.quoteAuthor.text = author
+        }
+        changeImage(count: count)
+    }
+    
+    func changeImage(count: Int) {
+        switch count{
+        case 0: fadeImage(newImage: #imageLiteral(resourceName: "bg0"))
+        case 1: fadeImage(newImage: #imageLiteral(resourceName: "bg1"))
+        case 2: fadeImage(newImage: #imageLiteral(resourceName: "bg2"))
+        case 3: fadeImage(newImage: #imageLiteral(resourceName: "bg3"))
+        default:
+            break
+        }
+    }
+    func fadeImage(newImage: UIImage) {
+        UIView.transition(with: self.bgImageView,
+                          duration: 0.3,
+                          options: .transitionCrossDissolve,
+                          animations: {
+                            self.bgImageView.image = newImage},
+                          completion: nil)
+    }
+
     
     func rotateQuote(){
-            let item = self.feed?.items?[count]
-            let author:String = (item!.title)!
-            var quoting = (item!.description)!
-            quoting = String(quoting.dropLast())
-            quoting = String(quoting.dropFirst())
-            let quote: String = quoting
-            DispatchQueue.main.async {
-                // ..and update the UI
-                self.quoteText.text = quote
-                self.quoteAuthor.text = author
-            }
-            if (count == 0){bgImageView.image = #imageLiteral(resourceName: "bg0")}
-            if (count == 1){bgImageView.image = #imageLiteral(resourceName: "bg1")}
-            if (count == 2){bgImageView.image = #imageLiteral(resourceName: "bg2")}
-            if (count == 3){bgImageView.image = #imageLiteral(resourceName: "bg3")
-                count = -1
-            }
-            count = (count + 1)
-
+        let item = self.feed?.items?[count]
+        let author:String = (item!.title)!
+        var quoting = (item!.description)!
+        quoting = String(quoting.dropLast())
+        quoting = String(quoting.dropFirst())
+        let quote: String = quoting
+        DispatchQueue.main.async {
+            // ..and update the UI
+            self.quoteText.text = quote
+            self.quoteAuthor.text = author
+        }
+        changeImage(count: count)
+        if (count == 3){count = -1}
+        count = (count + 1)
     }
     
     
@@ -223,56 +315,53 @@ class ViewController: UIViewController {
 
     }
     
-    func animateIn() {
+    func circleButton4(button: UIButton){
+        let borderAlpha : CGFloat = 0.7
+        button.frame = CGRect(x: 100,y: 100,width: 200,height: 40)
+        button.setTitle("CONTINUE", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.backgroundColor = UIColor.clear
+        button.layer.borderWidth = 1.75
+        button.layer.borderColor = UIColor(white: 0, alpha: borderAlpha).cgColor
+        button.layer.cornerRadius = 3 * shareButtonOutlet.frame.height / 5
+    }
+    
+    func animateIn(uiview: UIView) {
         visualEffectView.isHidden = false
-        self.view.addSubview(TimePickerViewOutlet)
-        TimePickerViewOutlet.center = self.view.center
+        self.view.addSubview(uiview)
+        uiview.center = self.view.center
         
-        TimePickerViewOutlet.transform = CGAffineTransform.init(scaleX: 1.3, y:1.3)
-        TimePickerViewOutlet.alpha = 0
-        
+//        uiview.transform = CGAffineTransform.init(scaleX: 1.3, y:1.3)
+        uiview.alpha = 0
+
         UIView.animate(withDuration: 0.4) {
             self.visualEffectView.effect = self.effect
-            self.TimePickerViewOutlet.alpha = 1
-            self.TimePickerViewOutlet.transform = CGAffineTransform.identity
+            uiview.alpha = 1
+//            uiview.transform = CGAffineTransform.identity
         }
     }
     
-    func animateOut() {
+    func animateInFrame(uiview: UIView) {
+        uiview.frame = CGRect(x: 0, y: 0, width: self.view.frame.width * 0.8, height: self.view.frame.height * 0.8)
+        animateIn(uiview: uiview)
+        
+    }
+    
+    func animateOut(uiview: UIView) {
         UIView.animate(withDuration: 0.3, animations: {
-            self.TimePickerViewOutlet.transform = CGAffineTransform.init(scaleX: 1.3, y:1.3)
-            self.TimePickerViewOutlet.alpha = 0
+            uiview.transform = CGAffineTransform.init(scaleX: 1.3, y:1.3)
+            uiview.alpha = 0
             
             self.visualEffectView.effect = nil
             
         })
         visualEffectView.isHidden = true
-    }
-    func animateIn2() {
-        visualEffectView.isHidden = false
-        self.view.addSubview(inAppViewOutlet)
-        inAppViewOutlet.center = self.view.center
+
         
-        inAppViewOutlet.transform = CGAffineTransform.init(scaleX: 1.3, y:1.3)
-        inAppViewOutlet.alpha = 0
-        
-        UIView.animate(withDuration: 0.4) {
-            self.visualEffectView.effect = self.effect
-            self.inAppViewOutlet.alpha = 1
-            self.inAppViewOutlet.transform = CGAffineTransform.identity
-        }
     }
     
-    func animateOut2() {
-        UIView.animate(withDuration: 0.3, animations: {
-            self.inAppViewOutlet.transform = CGAffineTransform.init(scaleX: 1.3, y:1.3)
-            self.inAppViewOutlet.alpha = 0
-            
-            self.visualEffectView.effect = nil
-            
-        })
-        visualEffectView.isHidden = true
-    }
+    
+    
     
     func Spoofify() {
         let yourAttributes : [NSAttributedStringKey: Any] = [
@@ -285,14 +374,15 @@ class ViewController: UIViewController {
         
         self.quoteText.alpha = 0
         self.quoteAuthor.alpha = 0
-        self.NextButtonOutlet.alpha = 0
         self.shareButtonOutlet.alpha = 0
         self.gearButtonOutlet.alpha = 0
         self.alarmButtonOutlet.alpha = 0
-        visualEffectInApp.layer.cornerRadius = 10;
+        visualEffectInApp.layer.cornerRadius = 10
         visualEffectTime.layer.cornerRadius = 10
+        newVisualOutlet.layer.cornerRadius = 10
         visualEffectTime.clipsToBounds = true
         visualEffectInApp.clipsToBounds = true
+        newVisualOutlet.clipsToBounds = true
         
         effect = visualEffectView.effect
         visualEffectView.effect = nil
@@ -301,7 +391,11 @@ class ViewController: UIViewController {
         circleButton()
         circleButton2()
         circleButton3()
+        circleButton4(button: newButtonOutlet)
+        
     }
+
+
     
     func playCoolIntro() {
         UIView.animate(withDuration: 1.5, delay: 0.5, animations: {
@@ -345,6 +439,15 @@ class ViewController: UIViewController {
             
         }
         return nil
+    }
+    
+    func isFirstTime() {
+        if UserDefaults.standard.bool(forKey: "showedWelcome") {
+        
+        } else {
+            self.animateIn(uiview: NewView)
+//            UserDefaults.standard.set(true, forKey: "showedWelcome")
+        }
     }
     
 }
